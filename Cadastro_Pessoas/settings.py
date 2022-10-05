8b3450f3c631321b81f41045152268c9d5c479ee
Dione <padilhadione1@gmail.com>
2022-10-05 17:48:20 -0300

    add deploy railway

--- Cadastro_Pessoas/settings.py
+++ Cadastro_Pessoas/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['site-projeto-jogos.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'site-projeto-jogos.herokuapp.com', 'cadastro-de-jogos.up.railway.app/']
 
 
 # Application definition

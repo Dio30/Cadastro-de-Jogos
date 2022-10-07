1a17daaf1d60700b00ae6d16efe464b467c12dfd
Dione <padilhadione1@gmail.com>
2022-10-07 12:10:50 -0300

    fix database12

--- Cadastro_Pessoas/settings.py
+++ Cadastro_Pessoas/settings.py
@@ -98,15 +98,12 @@ WSGI_APPLICATION = 'Cadastro_Pessoas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('name'), # nome do banco de dados
         'USER': config('user'), # nome do usuario do banco de dados
         'PASSWORD': config('password'), # senha do usuario do banco de dados
         'HOST': config('host'), # host do banco de dados
         'PORT': '5432',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
     }
 }
 
@@ -183,4 +180,4 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True, default=DATABASE_URL)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True, default=os.getenv('DATABASE_URL'))

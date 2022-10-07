aaf2f72a60a745208f459159434358ff9bb6d77b
Dione <padilhadione1@gmail.com>
2022-10-07 11:25:03 -0300

    fix database5

--- Cadastro_Pessoas/settings.py
+++ Cadastro_Pessoas/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'Cadastro_Pessoas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('name'), # nome do banco de dados
         'USER': config('user'), # nome do usuario do banco de dados
         'PASSWORD': config('password'), # senha do usuario do banco de dados
@@ -179,4 +179,4 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True, engine=None)

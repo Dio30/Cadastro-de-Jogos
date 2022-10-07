94694f4f8e0276c46532fbb25755f9d82da81c3e
Dione <padilhadione1@gmail.com>
2022-10-07 11:09:26 -0300

    fix database

--- Cadastro_Pessoas/settings.py
+++ Cadastro_Pessoas/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
 from decouple import config
+import dj_database_url
 
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
@@ -96,8 +97,12 @@ WSGI_APPLICATION = 'Cadastro_Pessoas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('name'), # nome do banco de dados
+        'USER': config('user'), # nome do usuario do banco de dados
+        'PASSWORD': config('password'), # senha do usuario do banco de dados
+        'HOST': config('host'), # host do banco de dados
+        'PORT': '5432',
     }
 }
 
@@ -172,3 +177,5 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)

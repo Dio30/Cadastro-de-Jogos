5d3ae5890c6b194d4497fd43c99f4e2d19a05da8
Dione <padilhadione1@gmail.com>
2022-10-10 15:14:34 -0300

    fix database20

--- Cadastro_Pessoas/settings.py
+++ Cadastro_Pessoas/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'Cadastro_Pessoas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('name'), # nome do banco de dados
         'USER': config('user'), # nome do usuario do banco de dados
         'PASSWORD': config('password'), # senha do usuario do banco de dados
@@ -106,8 +106,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-DATABASE_URL = 'postgresql://<postgresql>'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -180,4 +178,4 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True, default=os.getenv('DATABASE_URL'))
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)

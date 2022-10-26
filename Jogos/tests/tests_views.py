38bb05c97679e9e86c5b6e9917913899de1b18eb
Dione <padilhadione1@gmail.com>
2022-10-26 19:12:26 -0300

    add more CRUD tests

--- Jogos/tests/tests_views.py
+++ Jogos/tests/tests_views.py
@@ -1,7 +1,9 @@
+from typing import IO
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from ..models import Jogos
+import json
 
 class JogoViewTestCase(TestCase):
 # criando um usuario antes de fazer o test pois a view só pode ser acessada se o usuario estiver autenticado   
@@ -19,63 +21,64 @@ class JogoNewTestCase(TestCase):
     def setUp(self):
         User.objects.create_user(username='test', email='test@test.com', password='12345678a')
         self.client.login(username='test', password='12345678a')
-        user= User.objects.create(username='dione')
+        self.user= User.objects.create(username='dione')
         self.res = reverse("novo")
         self.jogo1 = Jogos.objects.create(
             nome_do_jogo='jogo1',
             estilo_do_jogo = 'Ação',
             imagem = 'static/imagens/74472.png',
-            usuario=user
+            usuario=self.user
             )
     
     def test_status_code_200_get(self):
-        res = reverse("novo")
-        resposta = self.client.get(res)
+        resposta = self.client.get(self.res)
         self.assertEquals(resposta.status_code, 200)
         self.assertTemplateUsed(resposta, 'jogos/jogos_form.html', msg_prefix='Não é esse Template')
     
     def test_status_code_200_post(self):
-        resposta = self.client.post(self.res, self.jogo1)
+        resposta = self.client.post(self.res, {
+            'nome_do_jogo': 'jogo1',
+            'estilo_do_jogo': 'Ação',
+            'imagem': 'static/imagens/74472.png',
+            'usuario': self.user
+        })
         self.assertEquals(resposta.status_code, 200)
-        self.assertTemplateUsed(resposta, 'jogos/jogos_form.html', msg_prefix='Não é esse Template')
-        
-class JogoEditTestCase(TestCase): #terminar
+        self.assertEquals(self.jogo1.nome_do_jogo.title().lower(), 'jogo1')
+
+class JogoEditTestCase(TestCase):
     def setUp(self):
         User.objects.create_user(username='test', email='test@test.com', password='12345678a')
         self.client.login(username='test', password='12345678a')
-        user= User.objects.create(username='dione')
+        self.user= User.objects.create(username='dione')
+        self.jogo1 = Jogos.objects.create(
+            nome_do_jogo='jogo1',
+            estilo_do_jogo = 'Ação',
+            imagem = 'static/imagens/74472.png',
+            usuario=self.user
+            )
+        self.res = reverse("novo")
+        
+    def test_status_code_200_put(self):
+        resposta = self.client.put(self.res, {
+            'nome_do_jogo': 'jogo2',
+            'estilo_do_jogo': 'Aventura',
+            'imagem': 'static/imagens/74472.png',
+            'usuario': self.user
+        })
+        self.assertEquals(resposta.status_code, 200)
+        self.assertTemplateUsed(resposta, 'jogos/jogos_form.html', msg_prefix='Não é esse Template')
+
+class JogoDeleteTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='dione')
         self.jogo1 = Jogos.objects.create(
             nome_do_jogo='jogo1',
             estilo_do_jogo = 'Ação',
             imagem = 'static/imagens/74472.png',
             usuario=user
             )
-        
-    def test_status_code_200(self):
-        res = reverse("editar", args=['jogo1'])
-        resposta = self.client.get(res)
-        self.assertEquals(resposta.status_code, 200)
-        self.assertTemplateUsed(resposta, 'jogos/jogos_form.html', msg_prefix='Não é esse Template')
+        self.res = reverse("deletar", kwargs={'pk':self.jogo1.pk})
     
-    def test_status_code_302(self):
-        res = reverse("editar", args=['jogo1'])
-        resposta = self.client.put(res, self.jogo1)
+    def test_status_code_204_delete(self):
+        resposta = self.client.delete(self.res)
         self.assertEquals(resposta.status_code, 302)
-        self.assertTemplateUsed(resposta, 'jogos/jogos_form.html', msg_prefix='Não é esse Template')
-        
-class JogoDeleteTestCase(TestCase): #terminar
-    def setUp(self):
-        User.objects.create_user(username='test', email='test@test.com', password='12345678a')
-        self.client.login(username='test', password='12345678a')
-        user= User.objects.create(username='dione')
-        self.jogo1 = Jogos.objects.create(
-            nome_do_jogo='jogo1',
-            estilo_do_jogo = 'Ação',
-            imagem = 'static/imagens/74472.png',
-            usuario=user
-            )
-      
-    def test_status_code_204(self):
-        user = reverse('deletar', args=['jogo1'])
-        resposta = self.client.delete(user, self.jogo1)
-        self.assertEquals(resposta.status_code, 204)

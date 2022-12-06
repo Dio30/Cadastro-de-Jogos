35298da393a73db6b6bcda5fa42c1dd1f64db1a0
Dione <padilhadione1@gmail.com>
2022-12-06 13:54:34 -0300

    add in settings min_lenght in auth password validator

--- Jogos/admin.py
+++ Jogos/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Jogos
 
 class JogosAdmin(admin.ModelAdmin):
-    list_display = ['nome_do_jogo', 'estoque', 'estilo_do_jogo', 'imagem', 'usuario']
+    list_display = ['nome_do_jogo', 'estilo_do_jogo', 'estoque', 'imagem', 'usuario']
     readonly_fields = ['usuario']
 
 admin.site.register(Jogos, JogosAdmin)
--- Jogos/models.py
+++ Jogos/models.py
@@ -13,7 +13,7 @@ escolhas = [
 
 class Jogos(models.Model):
     nome_do_jogo = models.CharField(max_length=200, unique=True, error_messages={"unique": ("Já existe um jogo com este nome!")})
-    estoque = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0),
+    estoque = models.PositiveIntegerField(default=0, verbose_name='Quant. em Estoque', validators=[MinValueValidator(0),
                                        MaxValueValidator(30, message='O valor maximo é de 30 em estoque!')])
     estilo_do_jogo = models.CharField(max_length=50, default='Outros', choices=escolhas, verbose_name='Estilo:')
     imagem = models.ImageField(upload_to='jogos', null=True, blank=True, verbose_name='Imagem:')

cbf167e0c466da3f3665e392479ec1d12220f585
Dione <padilhadione1@gmail.com>
2022-12-06 17:11:39 -0300

    fix tests

--- Jogos/tests/tests_forms.py
+++ Jogos/tests/tests_forms.py
@@ -3,17 +3,17 @@ from ..forms import JogosForm
 from django.contrib.auth.models import User
 
 class JogosFormTest(TestCase):
-    
-    def test_carros_form_valido(self):
+    def test_jogos_form_valido(self):
         user= User.objects.create(username='dione')
         form = JogosForm(data={
             'nome_do_jogo':'fifa',
+            'estoque':"1",
             'estilo_do_jogo':'Ação',
             'imagem': 'static/imagens/74472.png',
             "usuario": user,
         })
         self.assertTrue(form.is_valid())
     
-    def test_carros_form_invalido(self): #para saber se acontece erro no form com os dados vazios, se estiver retorna True
+    def test_jogos_form_invalido(self): #para saber se acontece erro no form com os dados vazios, se estiver retorna True
         form = JogosForm(data={})
         self.assertFalse(form.is_valid())
